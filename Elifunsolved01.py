8573b029a83d2b87619208805a7e09b712ecce28
Kumar Shourya Singh <shouryakiit@gmail.com>
2021-04-22 19:28:16 +0530

    Commit through Pycharm Prac Question

---
+++ Elifunsolved01.py
@@ -0,0 +1,12 @@
+User = input('Enter User Input:\n')
+
+if User == 'LOL':
+    print('LOL = laughing out loud')
+elif User == 'ROFL':
+    print('ROFL = rolling on the floor laughing')
+elif User == 'LMK':
+    print('LMK = Let Me Know')
+elif User == 'SMH':
+    print('SMH = Shaking my head')
+else:
+    print("Sorry, don't know that one")
